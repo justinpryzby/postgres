b1e611ca1f2495381b492e1b2469f95c089cec84
houzj.fnst@fujitsu.com <houzj.fnst@fujitsu.com>
2021-12-20 06:45:33 +0000

    relcache not invalidated when ADD PRIMARY KEY USING INDEX
    Hi,
    
    When reviewing some replica identity related patches, I found that when adding
    primary key using an existing unique index on not null columns, the
    target table's relcache won't be invalidated.
    
    This would cause an error When REPLICA IDENTITY is default and we are
    UPDATE/DELETE a published table , because we will refer to
    RelationData::rd_pkindex to check if the UPDATE or DELETE can be safety
    executed in this case.
    
    ---reproduction steps
    CREATE TABLE test(a int not null, b int);
    CREATE UNIQUE INDEX a ON test(a);
    CREATE PUBLICATION PUB for TABLE test;
    UPDATE test SET a = 2;
    	ERROR:  cannot update table "test" because it does not have a replica identity and publishes updates
    	HINT:  To enable updating the table, set REPLICA IDENTITY using ALTER TABLE.
    
    alter table test add primary key using index a;
    UPDATE test SET a = 2;
    	ERROR:  cannot update table "test" because it does not have a replica identity and publishes updates
    	HINT:  To enable updating the table, set REPLICA IDENTITY using ALTER TABLE.

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -2052,6 +2052,7 @@ index_constraint_create(Relation heapRelation,
 		HeapTuple	indexTuple;
 		Form_pg_index indexForm;
 		bool		dirty = false;
+		bool		primary_marked = false;
 
 		pg_index = table_open(IndexRelationId, RowExclusiveLock);
 
@@ -2065,6 +2066,7 @@ index_constraint_create(Relation heapRelation,
 		{
 			indexForm->indisprimary = true;
 			dirty = true;
+			primary_marked = true;
 		}
 
 		if (deferrable && indexForm->indimmediate)
@@ -2079,6 +2081,13 @@ index_constraint_create(Relation heapRelation,
 
 			InvokeObjectPostAlterHookArg(IndexRelationId, indexRelationId, 0,
 										 InvalidOid, is_internal);
+
+			/*
+			 * Invalidate the relcache for the table, so that after we commit
+			 * all sessions will refresh the table's primary key.
+			 */
+			if (primary_marked)
+				CacheInvalidateRelcache(heapRelation);
 		}
 
 		heap_freetuple(indexTuple);
